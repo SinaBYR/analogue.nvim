7f27e6c90a1215683f5c39f74adebbdb288d88f9
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2023-11-20 20:45:58 +0330

    add buffer_to_string util func

--- lua/analogue/util.lua
+++ lua/analogue/util.lua
@@ -6,5 +6,10 @@ function M.print_table(table)
 	vim.print(table)
 end
 
+function M.buffer_to_string()
+	local content = vim.api.nvim_buf_get_lines(0, 0, vim.api.nvim_buf_line_count(0), false)
+	return table.concat(content, "\n")
+end
+
 return M
 
