4ba48f564f0b88a38a8a199b675d83f2f2c887bb
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2023-11-20 20:46:25 +0330

    add buf and win default options

---
+++ lua/analogue/config.lua
@@ -0,0 +1,25 @@
+-- TODO find resize event
+-- TODO insert text into buffer
+
+
+local M = {}
+
+M.buf_opts = {
+	buftype = 'nofile'
+}
+
+M.win_opts = {
+	title = 'Sina',
+	title_pos = 'center',
+	-- focusable = false,
+	relative = 'editor', -- win, editor, cursor, mouse
+	style = 'minimal', -- removes row number
+	row = 10,
+	col = 80,
+	width = 20,
+	height = 10,
+	border = { "/", "-", "\\", "|" } -- { ';' } -- {"", "", "", ">", "", "", "", "<"} - { ';' } 
+}
+
+return M
+
